a5a4ba51b6945784196e77fae159598b058fa7cf
kamilDawidowski <kamil.dawidowski@student.wat.edu.pl>
2021-01-05 00:39:11 +0100

    Dodanie porównywarki dwóch społek

--- Str_2_main/urls.py
+++ Str_2_main/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.menu, name='menu'),
     path('odp/', views.odp, name="odp"),
     path('odp/odp', views.odp, name="odp"),
+    path('odp2', views.odp2, name="odp2"),
 
 ]
--- Str_2_main/views.py
+++ Str_2_main/views.py
@@ -24,15 +24,115 @@ def menu(request):
                                        spolka_data_skrot=data['Symbol']).exists() == False:
             Nazwy_spółek.objects.create(spolka_data_name=data['Nazwa'], spolka_data_skrot=data['Symbol'])
     baza = Nazwy_spółek.objects.all()
-    baza = {'data': baza}
+    baza = {'data': baza, }
 
     return render(request, "Strona_2.html", baza)
 
 
-def odp(request):
-    labels = []
-    data = []
+def odp2(request):
+    name_spolka_do_analizy_ver1 = request.GET["spółka_do_analizy_ver1"]
 
+    name_spolka_do_analizy_ver2 = request.GET["spółka_do_analizy_ver2"]
+
+
+
+    print(str(name_spolka_do_analizy_ver1))
+    print(str(name_spolka_do_analizy_ver2))
+
+    name2 = str(szukanie(name_spolka_do_analizy_ver2))
+    name1 = str(szukanie(name_spolka_do_analizy_ver1))
+
+    print(name1)
+    print(name2)
+
+    www_ver1 = 'https://stooq.pl/q/d/l/?s=' + name1 + '&i=d'
+    www_ver2 = 'https://stooq.pl/q/d/l/?s=' + name2 + '&i=d'
+
+    odp1 = download(www_ver1, name1)
+    odp2 = download(www_ver2, name2)
+
+    dane1 = pd.read_csv(odp1, sep=',',
+                        na_values=" ")
+    dane2 = pd.read_csv(odp2, sep=',',
+                        na_values=" ")
+
+    a = dane1;
+    b = dane2;
+
+    all_Data1 = []
+    all_Data2 = []
+
+    for i in range(a.shape[0]):
+        temp = a.iloc[i]
+        all_Data1.append(dict(temp))
+
+    for i in range(b.shape[0]):
+        temp = b.iloc[i]
+        all_Data2.append(dict(temp))
+
+    if (name1 or name2 != 'None'):
+        flaga = True
+    else:
+        flaga = False
+
+    baza = Nazwy_spółek.objects.all()
+
+    context = {
+        'flaga': flaga,
+        'data': baza,
+        'name1': name1,
+        'name2': name2,
+
+    }
+    if (flaga == True):
+        del all_Data1[-1]
+        del all_Data2[-1]
+
+        ver1 = Nazwy_spółek.objects.get(spolka_data_skrot=name1)
+        ver2 = Nazwy_spółek.objects.get(spolka_data_skrot=name2)
+
+        for data in all_Data1:
+            if Dane_spółek.objects.filter(spolka_name=ver1,
+                                          spolka_data=data["b'Data"]).exists() == False:
+                Dane_spółek.objects.create(spolka_name=ver1,
+                                           spolka_otwarcie=data['Otwarcie'],
+                                           spolka_najwyzszy=data['Najwyzszy'],
+                                           spolka_najnizszy=data['Najnizszy'],
+                                           spolka_zamkniecie=data['Zamkniecie'],
+                                           spolka_data=data["b'Data"])
+        for data in all_Data2:
+            if Dane_spółek.objects.filter(spolka_name=ver2,
+                                          spolka_data=data["b'Data"]).exists() == False:
+                Dane_spółek.objects.create(spolka_name=ver2,
+                                           spolka_otwarcie=data['Otwarcie'],
+                                           spolka_najwyzszy=data['Najwyzszy'],
+                                           spolka_najnizszy=data['Najnizszy'],
+                                           spolka_zamkniecie=data['Zamkniecie'],
+                                           spolka_data=data["b'Data"])
+
+
+
+        dane_1 = Dane_spółek.objects.all().filter(spolka_name=ver1)
+        dane_2 = Dane_spółek.objects.all().filter(spolka_name=ver2)
+
+        dane2 = {
+            'dane_1': dane_1,
+            'dane_2': dane_2,
+            'nazwa_1': name1,
+            'nazwa_2': name2,
+        }
+
+        # dane = Dane_spółek.objects.all()
+
+        return render(request, "Strona_3_ver2.html", dane2)
+    else:
+        return render(request, "Strona_2.html", context=context)
+
+
+
+
+
+def odp(request):
     name_spolka_do_analizy = request.GET["spółka_do_analizy"]
     name = str(szukanie(name_spolka_do_analizy))
     www = 'https://stooq.pl/q/d/l/?s=' + name + '&i=d'
@@ -55,6 +155,7 @@ def odp(request):
     context = {
         'flaga': flaga,
         'data': baza,
+        'name': name,
 
     }
 
@@ -75,10 +176,15 @@ def odp(request):
                                            spolka_data=data["b'Data"])
 
         dane = Dane_spółek.objects.all().filter(spolka_name=k)
+
+        dane2 = {
+            'dane': dane,
+            'nazwa': name,
+        }
+
         # dane = Dane_spółek.objects.all()
 
-
-        return render(request, "Strona_3.html", {'dane': dane})
+        return render(request, "Strona_3.html", dane2)
     else:
         return render(request, "Strona_2.html", context=context)
 
